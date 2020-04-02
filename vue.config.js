352e26da527a5307bec6fa2764732ffca77af307
code <1796979978@qq.com>
2020-04-02 20:31:43 +0800

    master

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    publicPath: './',
+    /*
+    //跨域
+    devServer:{
+        proxy:{
+            "/api":{
+                target:"后台地址",
+                ws:true, //启用websockets
+                changeOrigin:true,  //是否跨域
+                pathRewrite:{
+                    "^/api/":""
+                },
+            },
+        }
+    }
+*/
+}
